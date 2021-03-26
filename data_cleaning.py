ab9941148508401e4ace8ce726f3f86b513d5736
Allison Li <allisonli18710@gmail.com>
2021-03-26 16:26:08 -0400

    Save AITA comments as CSV. Create data cleaning

---
+++ data_cleaning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import emoji
+import re
+#import en_core_web_sm
+import spacy
+
+am_i_the_asshole = pd.read_csv('AmItheAsshole_comments_top1.csv')
--- reddit_scraper.py
+++ reddit_scraper.py
@@ -9,8 +9,10 @@ import pandas as pd
 
 # Access API
 reddit = praw.Reddit(client_id="",      # Enter client ID
-                    client_secret=""  # Enter client secret
-                    user_agent="Comment Scraper by u/sentiment-analyses")
+                    client_secret="",  # Enter client secret
+                    user_agent="Comment Scraper by u/sentiment-analyses",
+                    username="",
+                    password="")
 
 # Select subreddit(s) to scrape
 subreddit_list = ['AmItheAsshole']
@@ -34,5 +36,7 @@ for sub in subreddit_list:
             comments_dict["comment_parent_id"].append(comment.parent_id)
             comments_dict["comment_body"].append(comment.body)
             comments_dict["comment_link_id"].append(comment.link_id)
+    post_comments = pd.DataFrame(comments_dict)
+    post_comments.to_csv(sub + '_comments_top1.csv')
 
 # print(comments_dict["comment_body"])
